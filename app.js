06c30fd05547eb1a505cc2bda0b75a50acaef77c
Dominik Gromski <40479@student.atar.edu.pl>
2026-04-14 12:44:30 +0200

    feat: dodano CSS

--- app.js
+++ app.js
@@ -68,5 +68,24 @@ button.addEventListener("click", () => {
   input.value = "";
 });
 
+const toggleBtn = document.getElementById("theme-toggle");
+
+// załaduj zapisany tryb
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "☀️ Tryb";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "🌙 Tryb";
+  }
+});
+
 // start
 renderTasks();
